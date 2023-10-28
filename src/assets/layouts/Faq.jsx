b08a0734581bac37b105767e3369a3c9702f02c9
Shristi Gupta <gshristi40@gmail.com>
2023-10-28 17:50:18 +0530

    added details and summary image in faq

--- src/assets/layouts/Faq.jsx
+++ src/assets/layouts/Faq.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import { useState } from "react";
+import imageClosed from '/images/faq-arrow-down.svg'; 
  const Faq = (props) => {
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+
     return (
         <div>
-            <details
+            <details 
                 className="newui-disclosure font-bold w-full pr-4 py-4 md:p-4 "
             >
                 <summary
-                    className="flex cursor-pointer list-none items-center"
+                    className="flex cursor-pointer list-none items-center" onClick={()=>setIsDetailsOpen(!isDetailsOpen)}
                 >
                     <div
                         className="flex w-full flex-col sm:flex-row  md:justify-between"
@@ -16,6 +19,8 @@ import React from 'react';
                             {props.heading}
                         </span>
                     </div>
+                    <div className={`w-10 h-10 mr-2 ${!isDetailsOpen ? 'rotate-0' : 'rotate-180'}`} style={{backgroundImage: `url(${imageClosed})`, backgroundSize: "cover"}}></div>
+
                 </summary>
                 <p className="pt-4 text-base font-normal text-clay-blue-200 lg:text-xl">
                     {props.content}
