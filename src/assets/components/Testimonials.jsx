1cea0e2025ded23100d453e0a75841af870fda20
Shristi Gupta <gshristi40@gmail.com>
2023-10-31 00:17:51 +0530

    testimonial section done

--- src/assets/components/Testimonials.jsx
+++ src/assets/components/Testimonials.jsx
@@ -13,12 +13,26 @@ const Testimonials = () => {
                     name: "Mehak Batra, Founder"
                 },
                 {
+                    img_path: "/images/servicebell.jpeg",
+                    heading: "Find dream clients at scale",
+                    content: "Clay enables insanely targeted prospecting at scale. Skilled users can build their dream list with a fraction of the time and money historically required.",
+                    companyName: "ServiceBell",
+                    name: "Jacob Tuwiner, Growth"
+                },
+                {
                     img_path: "/images/recruit-qrew.jpeg",
-                    heading: "2. Double your team’s efficiency",
+                    heading: "3. Double your team’s efficiency",
                     content: "Using Clay, we have cut our sales team by 50% AND are landing more clients.",
                     companyName: "Recruit Qrew",
                     name: "Mehak Batra, Founder"
-                }
+                },
+                {
+                    img_path: "/images/recruit-qrew.jpeg",
+                    heading: "4. Double your team’s efficiency",
+                    content: "Using Clay, we have cut our sales team by 50% AND are landing more clients.",
+                    companyName: "Recruit Qrew",
+                    name: "Mehak Batra, Founder"
+                },
             ]} />
         </section>
     )
--- src/assets/layouts/Testimonial.jsx
+++ src/assets/layouts/Testimonial.jsx
@@ -3,50 +3,59 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 const Testimonial = ({ slides }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
-    
+
     const nextSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
-       console.log("next",currentSlide)
-       
-      };
-    
-      const prevSlide = () => {
+    };
+
+    const prevSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
-        console.log("right",currentSlide)
-      };
+    };
     return (
         <div className="slider">
             <div className="slides-container" >
-                {slides.map((slide,index) => (
-                    <>
-                        
-                        <div key={index} className={`slide max-w-6xl mx-auto px-5 md:px-0 rounded `}>
-                            <div className="bg-white p-8 md:p-7  border border-b-[0.625rem] mb-20 md:mb-0">
-                                <div className="items-center md:flex justify-between">
-                                    <div className="items-center gap-x-4 md:flex">
-                                        <img src={window.location.origin + slide.img_path} className="h-8 md:h-7" />
-                                        <p className="text-xl md:text-2xl pt-4 md:pt-0 font-medium md:font-semibold">{slide.heading}</p>
+                <div className={`slide max-w-6xl mx-auto px-5 md:px-0 rounded `}>
+                    <div className="bg-white p-8 md:p-7  border border-b-[0.625rem] mb-20 md:mb-0 flex justify-between items-start">
+                        {slides.map((slide, index) => (
+                            <>
+                                <div className={`${currentSlide !== index ? 'hidden' : 'block'}`} key={index}>
+                                    <div className={` md:flex items-center justify-between`}>
+                                        <div className="items-center gap-x-4 md:flex">
+                                            <img src={window.location.origin + slide.img_path} className="h-8 md:h-7" />
+                                            <p className="text-xl md:text-2xl pt-4 md:pt-0 font-medium md:font-semibold">{slide.heading}</p>
+                                        </div>
+
                                     </div>
-                                    <div className="md:flex hidden gap-x-1  text-clay-blue-200 font-semibold">
-                                        <button className="px-2" onClick={prevSlide}>&lt;</button>
-                                        <button className="px-2" onClick={nextSlide}>&gt;</button>
+                                    <p className="py-4 md:py-7">{slide.content}</p>
+                                    <div className="flex text-clay-blue-200 md:pb-7">
+                                        <span className="text-[0.9rem] md:text-base font-bold">{slide.companyName} &nbsp;</span>
+                                        <span> {slide.name}</span>
                                     </div>
                                 </div>
-                                <p className="py-4 md:py-7">{slide.content}</p>
-                                <div className="flex text-clay-blue-200 md:pb-7">
-                                    <span className="text-[0.9rem] md:text-base font-bold">{slide.companyName} &nbsp;</span>
-                                    <span> {slide.name}</span>
-                                </div>
-                            </div>
-                            <div className="md:hidden flex items-center justify-center">
-                                <div className="flex justify-center text-lg gap-x-1 bg-clay-blue-50 w-16 h-8 text-clay-blue-200 font-semibold">
-                                    <button className="px-2 left-btn"  >&lt;</button>
-                                    <button className="px-2 right-btn" >&gt;</button>
-                                </div>
-                            </div>
+                            </>
+                        ))}
+                        <div className="md:flex hidden shrink-0 text-clay-blue-200 font-semibold">
+                            <button className="px-1" onClick={prevSlide}>
+                                <img src={window.location.origin + "/images/faq-arrow-down.svg"} alt="" className='rotate-90 w-6' />
+                            </button>
+                            <button className="px-1" onClick={nextSlide}>
+                                <img src={window.location.origin + "/images/faq-arrow-down.svg"} alt="" className='-rotate-90 w-6' />
+
+                            </button>
                         </div>
-                    </>
-                ))}
+                    </div>
+                    <div className="md:hidden flex items-center justify-center">
+                        <div className="flex justify-center text-lg gap-x-1 bg-clay-blue-50 w-16 h-8 text-clay-blue-200 font-semibold">
+                            <button className="px-1" onClick={prevSlide}>
+                                <img src={window.location.origin + "/images/faq-arrow-down.svg"} alt="" className='rotate-90 w-6' />
+                            </button>
+                            <button className="px-1" onClick={nextSlide}>
+                                <img src={window.location.origin + "/images/faq-arrow-down.svg"} alt="" className='-rotate-90 w-6' />
+
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
